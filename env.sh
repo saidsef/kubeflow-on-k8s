644ee1ea8999464ab40d3a8e827ff0bfaf55bd81
Said Sef <saidsef@gmail.com>
2018-11-23 21:13:41 +0000

    init commit

---
+++ env.sh
@@ -0,0 +1,7 @@
+PLATFORM=k8s
+KUBEFLOW_REPO=/mnt/tmp/kubeflow-0.3.3
+KUBEFLOW_VERSION=master
+KUBEFLOW_KS_DIR=/mnt/tmp/kubeflow-0.3.3/scripts/data-pipeline/ks_app
+KUBEFLOW_DOCKER_REGISTRY=
+K8S_NAMESPACE=kubeflow
+KUBEFLOW_CLOUD=null
